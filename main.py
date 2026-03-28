2ea9ea9162fe414e4f19c54f8fd6bde2d5ebcdfa
Mohamed Amir Moutem <TuteurIA@users.noreply.huggingface.co>
2026-03-28 21:13:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import aibrain
-from memory import add_user
 
-def atlas_app(userinput, selected_grade, pdf_file = None):
-    # Save user message (optional)
-    add_user(userinput)
-
+def atlas_app(userinput, selected_grade, pdf_file = None, history=[]):
     # Route based on level
     if selected_grade == "3AC":
-        answer = aibrain.atlas3AC(userinput, pdf_file)
+        answer = aibrain.atlas3AC(userinput, pdf_file, history)
         for chunk in answer:
             yield chunk
 
     elif selected_grade == "TC":
-        answer = aibrain.atlasTC(userinput, pdf_file)
+        answer = aibrain.atlasTC(userinput, pdf_file, history)
         for chunk in answer:
             yield chunk
 
     elif selected_grade == "1BAC":
-        answer = aibrain.atlas1BAC(userinput, pdf_file)
+        answer = aibrain.atlas1BAC(userinput, pdf_file, history)
         for chunk in answer:
             yield chunk
 
     elif selected_grade == "2BAC":
-        answer = aibrain.atlas2BAC(userinput, pdf_file)
+        answer = aibrain.atlas2BAC(userinput, pdf_file, history)
         for chunk in answer:
             yield chunk
-
-    return "Please select a valid level."
+    else:
+        yield "Please select a valid level."
